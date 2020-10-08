a4a8586bea913bd93aa7b3c1e159b5468eab3be4
Edouard Piron <ed.piron@gmail.com>
2020-10-08 21:50:01 +0200

    update dirty attribute spec

--- spec/lib/dirty_seed/dirty_attribute_spec.rb
+++ spec/lib/dirty_seed/dirty_attribute_spec.rb
@@ -41,62 +41,42 @@ RSpec.describe DirtySeed::DirtyAttribute do
 
   describe '#type' do
     context 'when column is a boolean' do
-      let(:dirty_boolean) do
-        described_class.new(
-          dirty_model: dirty_model,
-          column: Alfa.columns.find { |c| c.name == 'boolean' }
-        )
-      end
       it 'returns :boolean' do
-        expect(dirty_boolean.type).to eq :boolean
+        sql_type = ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(type: :boolean)
+        column = ActiveRecord::ConnectionAdapters::Column.new('boolean', false, sql_type)
+        expect(described_class.new(column: column).type).to eq :boolean
       end
     end
 
     context 'when column is an integer' do
-      let(:dirty_integer) do
-        described_class.new(
-          dirty_model: dirty_model,
-          column: Alfa.columns.find { |c| c.name == 'integer' }
-        )
-      end
       it 'returns :integer' do
-        expect(dirty_integer.type).to eq :integer
+        sql_type = ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(type: :integer)
+        column = ActiveRecord::ConnectionAdapters::Column.new('integer', 1, sql_type)
+        expect(described_class.new(column: column).type).to eq :integer
       end
     end
 
     context 'when column is a decimal' do
-      let(:dirty_float) do
-        described_class.new(
-          dirty_model: dirty_model,
-          column: Alfa.columns.find { |c| c.name == 'decimal' }
-        )
-      end
       it 'returns :float' do
-        expect(dirty_float.type).to eq :float
+        sql_type = ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(type: :decimal)
+        column = ActiveRecord::ConnectionAdapters::Column.new('decimal', 1.0, sql_type)
+        expect(described_class.new(column: column).type).to eq :float
       end
     end
 
     context 'when column is a string' do
-      let(:dirty_string) do
-        described_class.new(
-          dirty_model: dirty_model,
-          column: Alfa.columns.find { |c| c.name == 'string' }
-        )
-      end
       it 'returns :string' do
-        expect(dirty_string.type).to eq :string
+        sql_type = ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(type: :string)
+        column = ActiveRecord::ConnectionAdapters::Column.new(:string, '', sql_type)
+        expect(described_class.new(column: column).type).to eq :string
       end
     end
 
     context 'when column is a sti type' do
-      let(:dirty_sti_type) do
-        described_class.new(
-          dirty_model: DirtySeed::DirtyModel.new(model: Foxtrot),
-          column: Foxtrot.columns.find { |c| c.name == 'type' }
-        )
-      end
-      it 'returns :sti_type' do
-        expect(dirty_sti_type.type).to eq :sti_type
+      it 'returns :string' do
+        sql_type = ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(type: :string)
+        column = ActiveRecord::ConnectionAdapters::Column.new('type', '', sql_type)
+        expect(described_class.new(column: column).type).to eq :sti_type
       end
     end
   end
